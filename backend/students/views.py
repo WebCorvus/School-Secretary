4559dc3508ed2f8a208483f73c0f9464f6c3f157
João Victor <joaovictorpinheiro2007@gmail.com>
2025-06-07 12:55:27 -0300

    Implement search feature in student data viewset

--- backend/students/views.py
+++ backend/students/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
@@ -9,8 +9,14 @@ from .serializers import StudentDataSerializer
 class StudentDataViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentDataSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = [
+        "full_name",
+        "registration_number",
+        "cpf",
+    ]
 
-    @action(detail=True, methods=["get"], url_path="data")
+    @action(detail=True, methods=["get"], url_path="download-presence")
     def data(self, request, pk=None):
         student = self.get_object()
         serializer = self.get_serializer(student)
