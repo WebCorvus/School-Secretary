c38a06866fd64361065ad4ce76bc7ae77b21cab0
João Victor <joaovictorpinheiro2007@gmail.com>
2025-08-06 18:52:23 -0300

    docker/compose: config containers to be more minimal and run in host

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,20 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
 import Header from "@/components/Header";
 import HorizontalLine from "@/components/HorizontalLine";
 import Footer from "@/components/Footer";
 
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "App da Secretaria Escolar",
@@ -29,7 +21,7 @@ export default function RootLayout({
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+				className={inter.className}
 			>
 				<Header />
 				<HorizontalLine />
