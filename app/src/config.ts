0171821be0d815c22ac781486c5dd8233594e083
JoaoVictor3DS <joaovictorpinheiro2007@gmail.com>
2025-08-25 11:12:21 -0300

    app: fix link order in config

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,8 +1,8 @@
 export const API_BASE_URL = "http://potential-eureka-4xpgjr6x9vrf77q7-8000.app.github.dev";
 export const LOGIN_BASE_URL = `${API_BASE_URL}/api/token/`;
 export const GROUP_BASE_URL = `${API_BASE_URL}/school/group/`;
-export const ITINERARY_BASE_URL = `${API_BASE_URL}/school/itinerary/`;
 export const STUDENT_BASE_URL = `${API_BASE_URL}/students/data/`;
+export const ITINERARY_BASE_URL = `${API_BASE_URL}/school/itinerary/`;
 export const PROFESSOR_BASE_URL = `${API_BASE_URL}/school/professor/`;
 export const SUBJECT_BASE_URL = `${API_BASE_URL}/school/subject/`;
 export const LESSON_BASE_URL = `${API_BASE_URL}/school/lesson/`;
