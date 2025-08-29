8fd322fbd8096e3e5359e8dab0c9ae8632c34e25
JoaoVictor3DS <joaovictorpinheiro2007@gmail.com>
2025-08-29 08:20:21 -0300

    app: decrease footer font size

--- app/src/components/Footer/index.tsx
+++ app/src/components/Footer/index.tsx
@@ -1,9 +1,13 @@
 export default function Footer() {
 	return (
 		<footer className="footer">
-			<p className="font-semibold">Secretaria Escolar - Escola Exemplo</p>
-			<p>Rua das Escolas, 123 - Centro, Cidade Exemplo/EE</p>
-			<p>secretaria@exemplo.exp | (00) 4002-8922</p>
+			<p className="font-semibold text-sm">
+				Secretaria Escolar - Escola Exemplo
+			</p>
+			<p className="text-sm">
+				Rua das Escolas, 123 - Centro, Cidade Exemplo/EE
+			</p>
+			<p className="text-sm">secretaria@exemplo.exp | (00) 4002-8922</p>
 		</footer>
 	);
 }
