876ef1013973fcaea374f195fac70a7f8c6853cb
João Victor <joaovictorpinheiro2007@gmail.com>
2025-09-01 17:12:24 -0300

    api: set default view permission class to isAuthenticated

--- api/School-Secretary/settings.py
+++ api/School-Secretary/settings.py
@@ -63,7 +63,8 @@ CORS_ALLOWED_ORIGINS = os.environ.get(
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
 
 
