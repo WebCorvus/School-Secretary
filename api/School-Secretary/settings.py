e9ad9f78d90c43a51d32a5a93711aa36f0050787
JoaoVictor3DS <joaovictorpinheiro2007@gmail.com>
2025-09-03 08:38:35 -0300

    api: add https version of each host

--- api/School-Secretary/settings.py
+++ api/School-Secretary/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
 ]
 CORS_ALLOW_ALL_ORIGINS = bool(os.environ.get("CORS_ALLOW_ALL_ORIGINS", True))
 CORS_ALLOWED_ORIGINS = os.environ.get(
-    "CORS_ALLOWED_ORIGINS", "https://127.0.0.1"
+    "CORS_ALLOWED_ORIGINS", "http://127.0.0.1,https://127.0.0.1"
 ).split(",")
 
 REST_FRAMEWORK = {
