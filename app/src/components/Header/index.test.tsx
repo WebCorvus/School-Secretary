83b623cb392bfd70980a1cf1d1e9747850bd8a9f
João Victor <joaovictorpinheiro2007@gmail.com>
2025-09-08 16:22:15 -0300

    app: improve header links testing with redirection verification

--- app/src/components/Header/index.test.tsx
+++ app/src/components/Header/index.test.tsx
@@ -6,14 +6,40 @@ import Header from './index'
 describe('Header', () => {
   it('renders the header', () => {
     render(<Header />)
-    expect(screen.getByRole('link', { name: 'Início' })).toBeInTheDocument()
-    expect(screen.getByRole('link', { name: 'Estudantes' })).toBeInTheDocument()
-    expect(screen.getByRole('link', { name: 'Professores' })).toBeInTheDocument()
-    expect(screen.getByRole('link', { name: 'Matérias' })).toBeInTheDocument()
-    expect(screen.getByRole('link', { name: 'Itinerários' })).toBeInTheDocument()
-    expect(screen.getByRole('link', { name: 'Turmas' })).toBeInTheDocument()
-    expect(screen.getByRole('link', { name: 'Horários' })).toBeInTheDocument()
-    expect(screen.getByRole('link', { name: 'Eventos' })).toBeInTheDocument()
-    expect(screen.getByRole('link', { name: 'Agenda' })).toBeInTheDocument()
+    const inicioLink = screen.getByRole('link', { name: 'Início' })
+    expect(inicioLink).toBeInTheDocument()
+    expect(inicioLink).toHaveAttribute('href', '/')
+
+    const estudantesLink = screen.getByRole('link', { name: 'Estudantes' })
+    expect(estudantesLink).toBeInTheDocument()
+    expect(estudantesLink).toHaveAttribute('href', '/students')
+
+    const professoresLink = screen.getByRole('link', { name: 'Professores' })
+    expect(professoresLink).toBeInTheDocument()
+    expect(professoresLink).toHaveAttribute('href', '/professors')
+
+    const materiasLink = screen.getByRole('link', { name: 'Matérias' })
+    expect(materiasLink).toBeInTheDocument()
+    expect(materiasLink).toHaveAttribute('href', '/subjects')
+
+    const itinerariosLink = screen.getByRole('link', { name: 'Itinerários' })
+    expect(itinerariosLink).toBeInTheDocument()
+    expect(itinerariosLink).toHaveAttribute('href', '/itineraries')
+
+    const turmasLink = screen.getByRole('link', { name: 'Turmas' })
+    expect(turmasLink).toBeInTheDocument()
+    expect(turmasLink).toHaveAttribute('href', '/groups')
+
+    const horariosLink = screen.getByRole('link', { name: 'Horários' })
+    expect(horariosLink).toBeInTheDocument()
+    expect(horariosLink).toHaveAttribute('href', '/lessons')
+
+    const eventosLink = screen.getByRole('link', { name: 'Eventos' })
+    expect(eventosLink).toBeInTheDocument()
+    expect(eventosLink).toHaveAttribute('href', '/events')
+
+    const agendaLink = screen.getByRole('link', { name: 'Agenda' })
+    expect(agendaLink).toBeInTheDocument()
+    expect(agendaLink).toHaveAttribute('href', '/agenda')
   })
 })
