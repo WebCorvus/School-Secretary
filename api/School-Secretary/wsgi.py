d9c091686b472fa359820e88710f2d91ac438bc3
JMBIGMAC <jmiguelfom@gmail.com>
2025-09-09 18:01:12 -0300

    Api: teste

--- api/School-Secretary/wsgi.py
+++ api/School-Secretary/wsgi.py
@@ -14,3 +14,20 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "School-Secretary.settings")
 
 application = get_wsgi_application()
+
+"""
+WSGI config for School-Secretary project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "School-Secretary.settings")
+
+application = get_wsgi_application()
