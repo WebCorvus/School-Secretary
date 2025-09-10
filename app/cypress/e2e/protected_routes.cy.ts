8eaed099c585b4ab5b3d583abe23735f5e20c075
JoaoVictor3DS <joaovictorpinheiro2007@gmail.com>
2025-09-10 11:35:30 -0300

    app: add cypress auth test to all routes

--- app/cypress/e2e/protected_routes.cy.ts
+++ app/cypress/e2e/protected_routes.cy.ts
@@ -6,11 +6,75 @@ describe("Protected Routes", () => {
 		cy.url().should("include", "/login");
 	});
 
-	it("should allow access to protected route if authenticated", () => {
+	it("should allow access to students route if authenticated", () => {
 		cy.login();
 		cy.getCookie("access").should("exist");
 		cy.getCookie("refresh").should("exist");
 		cy.visit("/students");
 		cy.url().should("include", "/students");
+	});
+
+	it("should allow access to professors route if authenticated", () => {
+		cy.login();
+		cy.getCookie("access").should("exist");
+		cy.getCookie("refresh").should("exist");
+		cy.visit("/professors");
+		cy.url().should("include", "/professors");
+	});
+
+	it("should allow access to subjects route if authenticated", () => {
+		cy.login();
+		cy.getCookie("access").should("exist");
+		cy.getCookie("refresh").should("exist");
+		cy.visit("/subjects");
+		cy.url().should("include", "/subjects");
+	});
+
+	it("should allow access to agenda route if authenticated", () => {
+		cy.login();
+		cy.getCookie("access").should("exist");
+		cy.getCookie("refresh").should("exist");
+		cy.visit("/agenda");
+		cy.url().should("include", "/agenda");
+	});
+
+	it("should allow access to events route if authenticated", () => {
+		cy.login();
+		cy.getCookie("access").should("exist");
+		cy.getCookie("refresh").should("exist");
+		cy.visit("/events");
+		cy.url().should("include", "/events");
+	});
+
+	it("should allow access to groups route if authenticated", () => {
+		cy.login();
+		cy.getCookie("access").should("exist");
+		cy.getCookie("refresh").should("exist");
+		cy.visit("/groups");
+		cy.url().should("include", "/groups");
+	});
+
+	it("should allow access to itineraries route if authenticated", () => {
+		cy.login();
+		cy.getCookie("access").should("exist");
+		cy.getCookie("refresh").should("exist");
+		cy.visit("/itineraries");
+		cy.url().should("include", "/itineraries");
+	});
+
+	it("should allow access to lessons route if authenticated", () => {
+		cy.login();
+		cy.getCookie("access").should("exist");
+		cy.getCookie("refresh").should("exist");
+		cy.visit("/lessons");
+		cy.url().should("include", "/lessons");
+	});
+
+	it("should allow access to agenda route if authenticated", () => {
+		cy.login();
+		cy.getCookie("access").should("exist");
+		cy.getCookie("refresh").should("exist");
+		cy.visit("/agenda");
+		cy.url().should("include", "/agenda");
 	});
 });
