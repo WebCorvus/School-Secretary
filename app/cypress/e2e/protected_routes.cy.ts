d09e9f50170c2717141aed4895dc3408aa870bb2
João Victor <joaovictorpinheiro2007@gmail.com>
2025-09-09 21:53:16 -0300

    app cypress: remove incorrect files to develop thoroughly and add initial testing

--- app/cypress/e2e/protected_routes.cy.ts
+++ app/cypress/e2e/protected_routes.cy.ts
@@ -1,15 +1,16 @@
 describe("Protected Routes", () => {
 	it("should redirect to login if not authenticated", () => {
-		cy.visit("/students"); // A protected route
-		cy.url().should("eq", Cypress.config().baseUrl + "/login");
 		cy.getCookie("access").should("not.exist");
+		cy.getCookie("refresh").should("not.exist");
+		cy.visit("/students");
+		cy.url().should("include", "/login");
 	});
 
 	it("should allow access to protected route if authenticated", () => {
-		cy.login("testuser", "testpassword"); // Log in using the custom command
-		cy.visit("/students"); // A protected route
-		cy.url().should("eq", Cypress.config().baseUrl + "/students");
+		cy.login();
 		cy.getCookie("access").should("exist");
-		cy.get("h1").contains("Estudantes"); // Assert some content on the protected page
+		cy.getCookie("refresh").should("exist");
+		cy.visit("/students");
+		cy.url().should("include", "/students");
 	});
 });
--- app/cypress/support/index.d.ts
+++ app/cypress/support/index.d.ts
@@ -1,9 +1,5 @@
 declare namespace Cypress {
-  interface Chainable<Subject = any> {
-    /**
-     * Custom command to log in a user programmatically.
-     * @example cy.login('username', 'password')
-     */
-    login(username: string, password: string): Chainable<any>;
-  }
+	interface Chainable<Subject = any> {
+		login(): Chainable<any>;
+	}
 }
