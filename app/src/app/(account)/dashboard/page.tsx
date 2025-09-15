6014da3b090500e762ae0e2e9158e055c9703ed5
JoaoVictor3DS <joaovictorpinheiro2007@gmail.com>
2025-09-15 11:05:33 -0300

    app: move sidebar and header to global layout

--- app/src/app/(account)/dashboard/page.tsx
+++ app/src/app/(account)/dashboard/page.tsx
@@ -1,35 +1,5 @@
 "use client";
 
-import { AppSidebar } from "@/components/AppSidebar";
-import { ThemeSwitcher } from "@/components/ThemeSwitcher";
-import {
-	SidebarInset,
-	SidebarProvider,
-	SidebarTrigger,
-} from "@/components/ui/sidebar";
-
 export default function Page() {
-	return (
-		<SidebarProvider>
-			<AppSidebar />
-			<SidebarInset>
-				<header className="flex h-14 shrink-0 items-center gap-2">
-					<div className="flex flex-1 items-center gap-2 px-3">
-						<SidebarTrigger />
-					</div>
-					<div className="ml-auto px-3">
-						<ThemeSwitcher />
-					</div>
-				</header>
-				<div className="flex flex-1 flex-col gap-4 p-4">
-					<div className="grid auto-rows-min gap-4 md:grid-cols-3">
-						<div className="bg-muted/50 aspect-video rounded-xl" />
-						<div className="bg-muted/50 aspect-video rounded-xl" />
-						<div className="bg-muted/50 aspect-video rounded-xl" />
-					</div>
-					<div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
-				</div>
-			</SidebarInset>
-		</SidebarProvider>
-	);
+	return <h1>Dashboard</h1>;
 }
--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,10 +1,14 @@
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
 import "./globals.css";
-
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import { AppSidebar } from "@/components/AppSidebar";
+import { ThemeSwitcher } from "@/components/ThemeSwitcher";
+import {
+	SidebarInset,
+	SidebarProvider,
+	SidebarTrigger,
+} from "@/components/ui/sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,9 +24,22 @@ export default function RootLayout({
 		<html lang="pt-br" suppressHydrationWarning>
 			<body className={`${inter.className} `}>
 				<ThemeProvider>
-					<main className="flex-grow overflow-y-auto">
-						{children}
-					</main>
+					<SidebarProvider>
+						<AppSidebar />
+						<SidebarInset>
+							<header className="flex h-14 shrink-0 items-center gap-2">
+								<div className="flex flex-1 items-center gap-2 px-3">
+									<SidebarTrigger />
+								</div>
+								<div className="ml-auto px-3">
+									<ThemeSwitcher />
+								</div>
+							</header>
+							<div className="flex flex-1 flex-col gap-4 p-4">
+								{children}
+							</div>
+						</SidebarInset>
+					</SidebarProvider>
 				</ThemeProvider>
 			</body>
 		</html>
