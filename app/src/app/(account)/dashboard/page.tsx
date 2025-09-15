f62c4d8aa20a387531ff8fbc57c499bfddce145d
JoaoVictor3DS <joaovictorpinheiro2007@gmail.com>
2025-09-15 10:18:09 -0300

    app: add header initial setup

--- app/src/app/(account)/dashboard/page.tsx
+++ app/src/app/(account)/dashboard/page.tsx
@@ -1,13 +1,7 @@
+"use client";
+
 import { AppSidebar } from "@/components/AppSidebar";
-import {
-	Breadcrumb,
-	BreadcrumbItem,
-	BreadcrumbLink,
-	BreadcrumbList,
-	BreadcrumbPage,
-	BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import { Separator } from "@/components/ui/separator";
+import { ThemeSwitcher } from "@/components/ThemeSwitcher";
 import {
 	SidebarInset,
 	SidebarProvider,
@@ -19,25 +13,13 @@ export default function Page() {
 		<SidebarProvider>
 			<AppSidebar />
 			<SidebarInset>
-				<header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-					<SidebarTrigger className="-ml-1" />
-					<Separator
-						orientation="vertical"
-						className="mr-2 data-[orientation=vertical]:h-4"
-					/>
-					<Breadcrumb>
-						<BreadcrumbList>
-							<BreadcrumbItem className="hidden md:block">
-								<BreadcrumbLink href="#">
-									Building Your Application
-								</BreadcrumbLink>
-							</BreadcrumbItem>
-							<BreadcrumbSeparator className="hidden md:block" />
-							<BreadcrumbItem>
-								<BreadcrumbPage>Data Fetching</BreadcrumbPage>
-							</BreadcrumbItem>
-						</BreadcrumbList>
-					</Breadcrumb>
+				<header className="flex h-14 shrink-0 items-center gap-2">
+					<div className="flex flex-1 items-center gap-2 px-3">
+						<SidebarTrigger />
+					</div>
+					<div className="ml-auto px-3">
+						<ThemeSwitcher />
+					</div>
 				</header>
 				<div className="flex flex-1 flex-col gap-4 p-4">
 					<div className="grid auto-rows-min gap-4 md:grid-cols-3">
