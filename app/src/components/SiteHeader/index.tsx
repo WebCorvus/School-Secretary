4c599962420e063747e48ced3a1157e193a38ec9
João Victor <joaovictorpinheiro2007@gmail.com>
2025-09-15 19:30:13 -0300

    app: add text "Secretaria Escolar" and separator to SiteHeader

--- app/src/components/SiteHeader/index.tsx
+++ app/src/components/SiteHeader/index.tsx
@@ -1,11 +1,17 @@
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { ThemeSwitcher } from "@/components/ThemeSwitcher";
+import { Separator } from "@/components/ui/separator";
 
 export default function SiteHeader() {
 	return (
 		<header className="flex h-14 shrink-0 items-center gap-2">
 			<div className="flex flex-1 items-center gap-2 px-3">
 				<SidebarTrigger />
+				<Separator
+					orientation="vertical"
+					className="mx-2 data-[orientation=vertical]:h-4"
+				/>
+				<h1 className="text-base font-medium">Secretaria Escolar</h1>
 			</div>
 			<div className="ml-auto px-3">
 				<ThemeSwitcher />
