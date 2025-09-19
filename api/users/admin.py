f9c7e5313d2f8933c5a6346acc6f147da3364e47
João Victor <joaovictorpinheiro2007@gmail.com>
2025-09-19 15:23:49 -0300

    api: remove unnecessary field from admin user creation

--- api/users/admin.py
+++ api/users/admin.py
@@ -5,6 +5,11 @@ from users.models import User
 class UserAdmin(admin.ModelAdmin):
     list_display = ("email", "name", "role", "is_active", "is_staff", "is_superuser")
     search_fields = ("email", "name")
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("name", "role")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+    )
 
 
 admin.site.register(User, UserAdmin)
