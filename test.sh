10757bf47d5273042de7ee4e811fed0e780a0267
João Victor <joaovictorpinheiro2007@gmail.com>
2025-09-22 14:35:12 -0300

    test.sh: add all comands to perform a general test

--- test.sh
+++ test.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+docker compose -f compose.test.yaml build
+docker compose -f compose.test.yaml up -d
+
+sleep 10
+
 docker compose -f compose.test.yaml exec -T api python manage.py test
 docker compose -f compose.test.yaml exec -T app npm run test
+
+docker compose -f compose.test.yaml down
