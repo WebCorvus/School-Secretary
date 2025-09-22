aec60e91dc077247d5e0376911e8ed40bd0dad1a
João Victor <joaovictorpinheiro2007@gmail.com>
2025-09-22 14:44:21 -0300

    test.sh and compose.test: polish

--- test.sh
+++ test.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
+echo "---Building---"
 docker compose -f compose.test.yaml build
+echo "---Running---"
 docker compose -f compose.test.yaml up -d
 
+echo "---Waiting---"
 sleep 10
 
+echo "---Testing API---"
 docker compose -f compose.test.yaml exec -T api python manage.py test
+echo "---Testing APP---"
 docker compose -f compose.test.yaml exec -T app npm run test
 
-docker compose -f compose.test.yaml down
+echo "---Stopping and Removing---"
+docker compose -f compose.test.yaml down -v
