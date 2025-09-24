25138f169f6938b433e9606a86fc2cab0dcb4a32
João Victor <joaovictorpinheiro2007@gmail.com>
2025-09-24 15:01:15 -0300

    test.sh and ci.yaml: fix api test db connection error and improve scripts

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+
 set -e
 
-docker compose up --build -d
+echo "---Starting Containers---"
+docker compose up -d --wait
+
+echo "---Showing Logs---"
+docker compose logs -f
+
+echo "---All Services are up and healthy---"
