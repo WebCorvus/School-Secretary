23735828ebbd0df9421507d4392b4430daafd4fb
João Victor <joaovictorpinheiro2007@gmail.com>
2025-10-01 18:38:18 -0300

    remove unusable test container files

--- test/cypress/e2e/about.cy.js
+++
@@ -1,6 +0,0 @@
-describe("About page", () => {
-    it("should render the page", () => {
-        cy.visit("/about");
-        cy.get("h1").should("contain.text", "Secretaria Escolar - Escola Exemplo");
-    });
-});
