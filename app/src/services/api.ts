04a8e05a5a9cc448a4940580892f159b03c0880d
João Victor <joaovictorpinheiro2007@gmail.com>
2025-10-24 21:56:34 -0300

    app: fix service removing redirections to unexistent /login

--- app/src/services/api.ts
+++ app/src/services/api.ts
@@ -4,47 +4,43 @@ import { getCookie, setCookie } from "cookies-next";
 const api = axios.create();
 
 api.interceptors.request.use(
-	(config: any) => {
-		const token = getCookie("access");
-		if (token) {
-			if (!config.headers) config.headers = {};
-			config.headers.Authorization = `Bearer ${token}`;
-		}
-		return config;
-	},
-	(error) => Promise.reject(error)
+  (config: any) => {
+    const token = getCookie("access");
+    if (token) {
+      if (!config.headers) config.headers = {};
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
 
 api.interceptors.response.use(
-	(response) => response,
-	async (error: any) => {
-		const originalRequest = error.config;
+  (response) => response,
+  async (error: any) => {
+    const originalRequest = error.config;
 
-		if (error.response?.status === 401 && !originalRequest._retry) {
-			originalRequest._retry = true;
-			try {
-				const response = await axios.post("/auth/refresh");
-				const { access } = response.data;
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const response = await axios.post("/auth/refresh");
+        const { access } = response.data;
 
-				setCookie("access", access, { path: "/", sameSite: "lax" });
+        setCookie("access", access, { path: "/", sameSite: "lax" });
 
-				if (!originalRequest.headers) originalRequest.headers = {};
-				originalRequest.headers.Authorization = `Bearer ${access}`;
+        if (!originalRequest.headers) originalRequest.headers = {};
+        originalRequest.headers.Authorization = `Bearer ${access}`;
 
-				console.log("Token refreshed!");
-				return api(originalRequest);
-			} catch (refreshError) {
-				console.error("Refresh token failed", refreshError);
-				return Promise.reject(refreshError);
-			}
-		}
+        console.log("Token refreshed!");
+        return api(originalRequest);
+      } catch (refreshError) {
+        console.error("Refresh token failed", refreshError);
+        return Promise.reject(refreshError);
+      }
+    }
 
-		if (error.response?.status === 403) {
-			console.error("Access error", error);
-		}
-
-		return Promise.reject(error);
-	}
+    return Promise.reject(error);
+  }
 );
 
 export default api;
