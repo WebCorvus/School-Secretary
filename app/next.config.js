e8cb23aa4f8c55febf491e247ce2362422ae6fb9
João Victor Pinheiro Reis <joaovictorpinheiro1510@gmail.com>
2026-09-06 19:30:29 -0300

    api: fix python version as 3.13

--- app/next.config.js
+++ app/next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone",
-    images: {
-        unoptimized: process.env.NODE_ENV === "development",
-    },
+	output: "standalone",
+	images: {
+		unoptimized: process.env.NODE_ENV === "development",
+	},
 };
 
 module.exports = nextConfig;
--- controller.sh
+++ controller.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-  echo "At least one argument is required."
-  exit 1
+    echo "At least one argument is required."
+    exit 1
 fi
 
 NO_CONFIRM=false
 SCRIPT_NAME=""
 
 for arg in "$@"; do
-  if [ "$arg" == "--no-confirm" ]; then
-    NO_CONFIRM=true
-  else
-    SCRIPT_NAME="$arg"
-  fi
+    if [ "$arg" == "--no-confirm" ]; then
+        NO_CONFIRM=true
+    else
+        SCRIPT_NAME="$arg"
+    fi
 done
 
 if [ -z "$SCRIPT_NAME" ]; then
-  echo "Script name is required."
-  exit 1
+    echo "Script name is required."
+    exit 1
 fi
 
 SCRIPT_DIR="$(dirname "$(realpath "$0")")/scripts"
 SCRIPT_PATH="$SCRIPT_DIR/$SCRIPT_NAME.sh"
 
 if [ ! -f "$SCRIPT_PATH" ]; then
-  echo "'$SCRIPT_NAME.sh' not found in 'scripts/'."
-  exit 1
+    echo "'$SCRIPT_NAME.sh' not found in 'scripts/'."
+    exit 1
 fi
-
 
 echo "-------------------------"
 echo "Executing $SCRIPT_NAME..."
@@ -42,8 +41,8 @@ else
 fi
 
 if [ "$NO_CONFIRM" = false ]; then
-  chmod +x "$SCRIPT_DIR/on_exit.sh"
-  "$SCRIPT_DIR/on_exit.sh"
+    chmod +x "$SCRIPT_DIR/on_exit.sh"
+    "$SCRIPT_DIR/on_exit.sh"
 fi
 
 exit 0
